4777d935c98e6d5d17930009c0c1ff7256dba3d4
ramshaanadeem <ramshaanadeem5@gmail.com>
2021-07-26 15:23:08 +0500

    build|refactor: text rendering at game over, calculation of scores and code refactoring

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,65 +1,40 @@
 extern crate piston_window;
 extern crate rand;
-// use rand::{Rng};
-
 mod draw;
 mod game;
 mod snake;
-
+use crate::draw::to_coord_u32;
+use crate::game::Game;
 use piston_window::types::Color;
 use piston_window::*;
 
-use crate::draw::to_coord_u32;
-use crate::game::{Game};
-// use crate::game::Block;
-// #[derive(Copy)]
-// pub struct Block {
-//     x:i32,
-//     y:i32,
-// }
 const BACK_COLOR: Color = [0.0, 0.0, 0.0, 1.0];
-// const BORDER_COLOR: Color = [0.5, 0.50, 0.50, 1.0];
 
 fn main() {
     let (width, height) = (20, 20);
 
     let mut window: PistonWindow =
-        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
+        WindowSettings::new("Snake Game", [to_coord_u32(width), to_coord_u32(height)])
             .exit_on_esc(true)
             .build()
             .unwrap();
 
-
     let mut game = Game::new(width, height);
-    // creating obstacles
-    // let no_of_obstacles = rand::thread_rng().gen_range(1..5);
-    // println!("{}",no_of_obstacles);
-    // let mut obstacles : Vec<game::Block> = Vec::new();
-    // for _i in 0..no_of_obstacles {
-    //     let x_coord = rand::thread_rng().gen_range(1..19);
-    //     let y_coord = rand::thread_rng().gen_range(1..19);
-        
-    //     // draw_block(BORDER_COLOR, x_coord, y_coord, con, g);
-    //     obstacles.push( Block {
-    //         x: x_coord,
-    //         y: y_coord,
-    //     });
-    
-    // }
+    let mut glyphs = Glyphs::from_bytes(
+        include_bytes!("Roboto-Black.ttf"),
+        window.create_texture_context(),
+        TextureSettings::new(),
+    )
+    .unwrap();
     while let Some(event) = window.next() {
         if let Some(Button::Keyboard(key)) = event.press_args() {
             game.key_pressed(key);
         }
-        window.draw_2d(&event, |c, g, _| {
+        window.draw_2d(&event, |c, g, device| {
             clear(BACK_COLOR, g);
-            game.draw(&c, g);
-
+            game.draw(&mut glyphs, &c, g);
+            glyphs.factory.encoder.flush(device);
         });
-        // for i in 0..no_of_obstacles - 1 {
-        //     window.draw_2d(&event, |c, g, _| {
-        //         game.draw_obstacles(obstacles[i].x, obstacles[i].y, &c, g)
-        //     } );
-        // }
         event.update(|arg| {
             game.update(arg.dt);
         });
